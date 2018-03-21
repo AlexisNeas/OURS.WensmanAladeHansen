635ad36be14cea514b34c73e4f5a82d66d8ec9a6
TWENSMAN001 <twensman001@vplincsci087.ad.csbsju.edu>
2018-03-21 15:16:10 -0500

    OURS.WensmanAladeHansen

--- src/edu/csbsju/cs/SimplePrint.java
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -2,57 +2,57 @@ package edu.csbsju.cs;
 
 public class SimplePrint {
 
-	private String pName;
-	private int pAge;
+ private String pName;
+ private int pAge;
 
 
-	/**
-	 * @return the pName
-	 */
-	public String getpName() {
-		return pName;
-	}
+ /**
+  * @return the pName
+  */
+ public String getpName() {
+  return pName;
+ }
 
-	/**
-	 * @param pName
-	 * @param pAge
-	 */
-	public SimplePrint(String pName, int pAge) {
-		super();
-		this.pName = pName;
-		this.pAge = pAge;
-	}
+ /**
+  * @param pName
+  * @param pAge
+  */
+ public SimplePrint(String pName, int pAge) {
+  super();
+  this.pName = pName;
+  this.pAge = pAge;
+ }
 
-	/**
-	 * @param pName the pName to set
-	 */
-	public void setpName(String pName) {
-		this.pName = pName;
-	}
+ /**
+  * @param pName the pName to set
+  */
+ public void setpName(String pName) {
+  this.pName = pName;
+ }
 
-	/**
-	 * @return the pAge
-	 */
-	public int getpAge() {
-		return pAge;
-	}
+ /**
+  * @return the pAge
+  */
+ public int getpAge() {
+  return pAge;
+ }
 
-	/**
-	 * @param pAge the pAge to set
-	 */
-	public void setpAge(int pAge) {
-		this.pAge = pAge;
-	}
+ /**
+  * @param pAge the pAge to set
+  */
+ public void setpAge(int pAge) {
+  this.pAge = pAge;
+ }
 
-	public SimplePrint() {
-		// TODO Auto-generated constructor stub
-	}
+ public SimplePrint() {
+  // TODO Auto-generated constructor stub
+ }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i=0; i<args.length;i++){
-			System.out.println("args[ " + i + " ] = " + args[i] + "we say hi");
-			}
-	}
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  for(int i=0; i<args.length;i++){
+   System.out.println("args[ " + i + " ] = " + args[i] + "NO");
+   }
+ }
 
 }
