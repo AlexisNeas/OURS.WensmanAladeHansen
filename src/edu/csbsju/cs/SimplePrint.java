6b8d801ee833b4e436e3f4c5203b75caede80a40
ZVETTER001 <zvetter001@vplincsci005.ad.csbsju.edu>
2018-03-21 15:12:13 -0500

    our changes

--- src/edu/csbsju/cs/SimplePrint.java
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -51,7 +51,7 @@ public class SimplePrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0; i<args.length;i++){
-			System.out.println("args[ " + i + " ] = " + args[i] );
+			System.out.println("args[ " + i + " ] = " + args[i] + "we say hi");
 			}
 	}
 
